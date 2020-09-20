de9452bcafb1c6d768d33831b45f210dcfe5d2e1
dhanlal <dhanlal@valuebound.com>
2020-09-21 03:24:50 +0530

    inital committ

---
+++ build/precache-manifest.ce3fc24714f699bc344445c31996f524.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2c33f4d88984cbc6c01930253a44cf5",
+    "url": "/XT-Coding-Assignment/index.html"
+  },
+  {
+    "revision": "bb5411a7d186291582ca",
+    "url": "/XT-Coding-Assignment/static/css/main.305e4246.chunk.css"
+  },
+  {
+    "revision": "30194943f38e82058f34",
+    "url": "/XT-Coding-Assignment/static/js/2.6dc0b447.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/XT-Coding-Assignment/static/js/2.6dc0b447.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb5411a7d186291582ca",
+    "url": "/XT-Coding-Assignment/static/js/main.849352c8.chunk.js"
+  },
+  {
+    "revision": "dfa19487114f653e5c1e",
+    "url": "/XT-Coding-Assignment/static/js/runtime-main.47662121.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/XT-Coding-Assignment/static/media/logo.5d5d9eef.svg"
+  }
+]);
